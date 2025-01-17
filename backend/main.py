26dd7cdbaea820533d424c72959a72c1f7ee11bb
vn10091 <vn10091@heb.com>
2025-01-17 16:41:10 -0600

    Created a Blog and User route

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
 from core.config import settings
-from db.session import engine
-from db.base import Base
+from apis.base import api_router
 
-def create_tables():
-    Base.metadata.create_all(bind=engine) 
+def include_router(app):
+    app.include_router(api_router)
 
-def start_application(): #entry point, responsable for creating tables and returning to app
-    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
-    create_tables()
-    return app
+#entry point, responsable for creating tables and returning to app
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+    include_router(app)
+    return app 
 
 app = start_application()
-
-
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
 @app.get("/")
 def hello():
     return {"msg": "Hello FastAPI"}
+
+
+
+
+
