dd4af2b7e8ec0534cf0eab66e71b569eff034504
vn10091 <vn10091@heb.com>
2025-01-17 12:20:10 -0600

    Section 3

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
+
+def create_tables():
+    Base.metadata.create_all(bind=engine) 
+
+def start_application(): #entry point, responsable for creating tables and returning to app
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
+
 
 app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
