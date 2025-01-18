069ce8571cf31cab4d80961d322e16867c32491e
vn10091 <vn10091@heb.com>
2025-01-18 14:44:27 -0600

    created a home page with a navbar

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,30 @@
 from fastapi import FastAPI
 from core.config import settings
 from apis.base import api_router
+from apps.base import app_router
+from fastapi.staticfiles import StaticFiles
+
 
 def include_router(app):
     app.include_router(api_router)
+    app.include_router(app_router)
+
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+   
 
 #entry point, responsable for creating tables and returning to app
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
     return app 
 
 app = start_application()
 
-@app.get("/")
-def hello():
-    return {"msg": "Hello FastAPI"}
+# @app.get("/")
+# def hello():
+#     return {"msg": "Hello FastAPI"}
 
 
 
