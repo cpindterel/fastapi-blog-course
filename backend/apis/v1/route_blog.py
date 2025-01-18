79ce1dbd6ed0d6cb64ecae81b9eb50d963ab8db9
vn10091 <vn10091@heb.com>
2025-01-18 12:44:50 -0600

    JWT auth

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -2,6 +2,8 @@ from typing import List
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from db.session import get_db
+from db.models.user import User
+from apis.v1.route_login import get_current_user
 from schemas.blog import CreateBlog, ShowBlog, UpdateBlog
 from db.repository.blog import create_new_blog, retreive_blog, list_blogs, update_blog_by_id, delete_blog_by_id
 
@@ -26,10 +28,10 @@ def get_all_blogs(db: Session = Depends(get_db)):
     return blogs
 
 @router.put("/{id}", response_model=ShowBlog)
-def update_a_blog(id:int,blog: UpdateBlog, db: Session = Depends(get_db)):
-    blog = update_blog_by_id(id=id, blog=blog, db=db, author_id =1)
-    if not blog:
-        raise HTTPException(detail = f"Blog with id {id} does not exist")
+def update_a_blog(id:int,blog: UpdateBlog, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    blog = update_blog_by_id(id=id, blog=blog, db=db, author_id =current_user.id)
+    if isinstance(blog, dict):
+        raise HTTPException(detail=blog.get("error"), status_code=status.HTTP_400_BAD_REQUEST)
     return blog
 
 @router.delete("/{id}")
